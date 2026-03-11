1430b26792b1a8f704260846085194c5c03066e9
arshumarkhan <arshumarkhan4@gmail.com>
2026-03-11 21:23:09 +0530

    Logistic-Regression.py

---
+++ Logistic-Regression.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+X = np.array([[1], [2], [3], [4], [5], [6]])
+
+y = np.array([0, 0, 0, 1, 1, 1])
+
+model = LogisticRegression()
+
+model.fit(X, y)
+
+hours = np.array([[3.5]])
+prediction = model.predict(hours)
+
+print("Prediction (0 = Fail, 1 = Pass):", prediction[0])
