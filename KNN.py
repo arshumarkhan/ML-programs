d8e9e688bb7da55b93b04b7e473947b0e74247dc
arshumarkhan <arshumarkhan4@gmail.com>
2026-04-20 11:38:47 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -1,28 +1,28 @@
-# Import libraries
+
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
-# Load dataset
+
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-# Split data
+
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Create KNN model
+
 model = KNeighborsClassifier(n_neighbors=3)
 
-# Train model (KNN just stores data)
+
 model.fit(X_train, y_train)
 
-# Predict
+
 y_pred = model.predict(X_test)
 
-# Accuracy
+
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
